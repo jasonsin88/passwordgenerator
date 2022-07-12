59dc9aa1785fefc7dcd468e0c489fa3ae42128e6
Sin <chunngai.sin@aruplab.com>
2022-07-12 14:59:17 -0600

    Password now generating

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
 // Assignment code here
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-var lowerAlph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperAlph = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialChar = "!#$%&'()*+,-./<=>?@[]^_`{|}~";
+var number = "0123456789";
+var lowerAlph = "abcdefghijklmnopqrstuvwxyz";
+var upperAlph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordChar = ""; 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword() {
+  
   var pLength = prompt("Plese enter number of characters desired for your password. The password must be between 8 to 128 characters.");
   if (pLength < 8 || pLength > 128) {
     alert("Please enter a number between 8 and 128");
@@ -26,58 +28,71 @@ function generatePassword() {
     var num = confirm("Would you like the password to include numbers?");
     var upper = confirm("Would you like the password to include uppercase letters?");
     var lower = confirm("Would you like the password to include lowercase letter?");
-  };
+  }
 
-  var passwordChar; 
-  if (special && num && upper && lower) {
+  else if (special && num && upper && lower) {
     passwordChar = specialChar.concat(number, lowerAlph, upperAlph);
   }
+
   else if (special && num && upper) {
     passwordChar = specialChar.concat(number, upperAlph);
   }
+
   else if (special && num && lower) {
     passwordChar = specialChar.concat(number, lowerAlph);
   }
+
   else if (special && upper && lower) {
     passwordChar = specialChar.concat(upperAlph, lowerAlph);
   }
-  else if (number && upper && lower) {
-    passwordChar = num.concat(upperAlph, lowerAlph);
+
+  else if (num && upper && lower) {
+    passwordChar = number.concat(upperAlph, lowerAlph);
   }
+
   else if (special && num) {
     passwordChar = specialChar.concat(number);
   }
+
   else if (special && upper) {
     passwordChar = specialChar.concat(upperAlph);
   }
+
   else if (special && lower) {
     passwordChar = specialChar.concat(lowerAlph);
   }
+
   else if (num && upper) {
     passwordChar = number.concat(upperAlph);
   }
+
   else if (num && lower) {
     passwordChar = number.concat(lowerAlph);
   }
+
   else if (upper && lower) {
     passwordChar = upperAlph.concat(lowerAlph);
   }
+
   else if (special) {
     passwordChar = specialChar;
   }
+
   else if (num) {
     passwordChar = number;
   }
+
   else if (upper) {
     passwordChar = upperAlph;
   }
+
   else if (lower) {
     passwordChar = lowerAlph;
   }
 
-  var passwordRandom = [];
+  var passwordRandom = "";
   for (var i = 0; i < pLength; i++) {
-    passwordRandom = passwordChar[Math.floor(Math.random() * pLength.length)];
+    passwordRandom = passwordRandom + passwordChar[Math.floor(Math.random() * pLength)];
   }
     return passwordRandom;
 };
